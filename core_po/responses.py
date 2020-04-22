aad2effdfa15b0090e8ca024ca4e61a51df55283
Victor Torres <vpaivatorres@gmail.com>
2020-04-22 17:07:35 -0300

    fix typo

--- core_po/responses.py
+++ core_po/responses.py
@@ -5,7 +5,7 @@ import attr
 class HTMLResponse:
     """Represents a basic Response.
 
-    Every response should contain an URL and have a text content.
+    Every response should contain a URL and have a text content.
     """
     url: str
     content: str
