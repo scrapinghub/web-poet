172af53f1d3933b60b294d9128f92207d8e1ecc2
Victor Torres <vpaivatorres@gmail.com>
2020-04-24 15:24:12 -0300

    fix typo

--- core_po/page_inputs.py
+++ core_po/page_inputs.py
@@ -3,6 +3,6 @@ import attr
 
 @attr.s(auto_attribs=True)
 class ResponseData:
-    """Represents response containing its URL and HTML content."""
+    """Represents a response containing its URL and HTML content."""
     url: str
     html: str
