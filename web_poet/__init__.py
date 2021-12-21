7495b5843be09f8f58c81b46e430d302e822172d
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2021-12-21 13:54:38 +0800

    add missing import for find_page_object_overrides

--- web_poet/__init__.py
+++ web_poet/__init__.py
@@ -1,3 +1,3 @@
 from .pages import WebPage, ItemPage, ItemWebPage, Injectable
 from .page_inputs import ResponseData
-from .overrides import handle_urls, PageObjectRegistry
+from .overrides import handle_urls, find_page_object_overrides, PageObjectRegistry
