9ae60e64a7068870698f2a86c5ef1b70429f87fb
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-02-18 13:54:50 +0800

    add tests for Meta

--- tests/test_page_inputs.py
+++ tests/test_page_inputs.py
@@ -1,4 +1,4 @@
-from web_poet.page_inputs import ResponseData
+from web_poet.page_inputs import ResponseData, Meta
 
 
 def test_html_response():
@@ -11,3 +11,23 @@ def test_html_response():
     response = ResponseData("url", "content", 200, {"User-Agent": "test agent"})
     assert response.status == 200
     assert response.headers["User-Agent"] == "test agent"
+
+
+def test_meta():
+    meta = Meta(x="hi", y=2.2, z={"k": "v"})
+    assert meta.x == "hi"
+    assert meta.y == 2.2
+    assert meta.z == {"k": "v"}
+    assert meta.not_existing_field is None
+
+    del meta.z
+    assert meta.z is None
+
+    # Deleting non-existing fields should not err out.
+    del meta.no_existing_field
+    assert meta.not_existing_field is None
+
+    meta.new_field = "new"
+    assert meta.new_field == "new"
+
+    str(meta) == "Meta(x='hi', y=2.2, new='new')"
