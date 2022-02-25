4b8c44647d80e94d4ea1549746273ce426c3d47d
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-02-25 17:05:17 +0800

    add DelegateFallback exception

--- web_poet/exceptions.py
+++ web_poet/exceptions.py
@@ -10,4 +10,20 @@ class ReloadResponseData(Exception):
     handles the Page Objects should then attempt to create a new instance with a
     fresh :class:`~.ResponseData` dependency.
     """
+
+    pass
+
+
+class DelegateFallback(Exception):
+    """Indicates that the Page Object isn't able to perform data extractions on
+    a given page.
+
+    This should be raised in cases wherein the page has unknown layouts, unsupported
+    data, etc.
+
+    Raising this won't be a guarantee that a fallback parser for the page is
+    available. It would depend on the framework using the Page Object on how to
+    resolve the fallbacks.
+    """
+
     pass
