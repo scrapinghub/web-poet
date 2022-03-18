3e9b8279ce927113cc6b008f56e1fbfd9fb5efe0
Mikhail Korobov <kmike84@gmail.com>
2022-03-18 18:27:02 +0500

    more tests for HttpResponse

--- tests/test_page_inputs.py
+++ tests/test_page_inputs.py
@@ -42,7 +42,7 @@ def test_http_response_body_json():
     assert http_body.json() == {"ключ": "значение"}
 
 
-def test_html_response():
+def test_http_response_defaults():
     http_body = HttpResponseBody(b"content")
 
     response = HttpResponse("url", body=http_body)
@@ -52,8 +52,34 @@ def test_html_response():
     assert not response.headers
     assert response.headers.get("user-agent") is None
 
+
+def test_http_response_with_headers():
+    http_body = HttpResponseBody(b"content")
     headers = HttpResponseHeaders.from_name_value_pairs([{"name": "User-Agent", "value": "test agent"}])
     response = HttpResponse("url", body=http_body, status=200, headers=headers)
     assert response.status == 200
     assert len(response.headers) == 1
     assert response.headers.get("user-agent") == "test agent"
+
+
+def test_http_response_bytes_body():
+    response = HttpResponse("http://example.com", body=b"content")
+    assert isinstance(response.body, HttpResponseBody)
+    assert response.body == HttpResponseBody(b"content")
+
+
+def test_http_response_body_validation_str():
+    with pytest.raises(TypeError):
+        response = HttpResponse("http://example.com", body="content")
+
+
+def test_http_response_body_validation_None():
+    with pytest.raises(TypeError):
+        response = HttpResponse("http://example.com", body=None)
+
+
+@pytest.mark.xfail(reason="not implemented")
+def test_http_response_body_validation_other():
+    with pytest.raises(TypeError):
+        response = HttpResponse("http://example.com", body=123)
+
