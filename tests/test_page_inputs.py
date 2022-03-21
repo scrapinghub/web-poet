cff773c0e9a734310e98ae4f792b6cf514c90315
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-03-21 17:03:04 +0800

    Add value restriction logic to Meta
    This reverts commit 079ccc1792649b9f3b5ac1ab4a26dd4849e85aa9.
    
    Revert "remove value restriction of Meta"
    
    This reverts commit a8d953023684375ab8bb319d15162bace35d02a8.

--- tests/test_page_inputs.py
+++ tests/test_page_inputs.py
@@ -1,4 +1,7 @@
-from web_poet.page_inputs import ResponseData
+import pytest
+import asyncio
+
+from web_poet.page_inputs import ResponseData, Meta
 
 
 def test_html_response():
@@ -11,3 +14,19 @@ def test_html_response():
     response = ResponseData("url", "content", 200, {"User-Agent": "test agent"})
     assert response.status == 200
     assert response.headers["User-Agent"] == "test agent"
+
+
+def test_meta_restriction():
+    # Any value that conforms with `Meta.restrictions` raises an error
+    with pytest.raises(ValueError) as err:
+        Meta(func=lambda x: x + 1)
+
+    with pytest.raises(ValueError) as err:
+        Meta(class_=ResponseData)
+
+    # These are allowed though
+    m = Meta(x="hi", y=2.2, z={"k": "v"})
+    m["allowed"] = [1, 2, 3]
+
+    with pytest.raises(ValueError) as err:
+        m["not_allowed"] = asyncio.sleep(1)
