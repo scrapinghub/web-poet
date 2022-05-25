e8a03c8fdc7c517facde2a2483d9a1f6bfeeecaa
Mikhail Korobov <kmike84@gmail.com>
2022-05-26 00:19:06 +0500

    [wip] web_poet.page_object decorator

--- web_poet/__init__.py
+++ web_poet/__init__.py
@@ -11,7 +11,7 @@ from .page_inputs import (
     HttpResponseBody,
 )
 from .overrides import PageObjectRegistry, consume_modules, OverrideRule
-
+from .functional import page_object
 
 default_registry = PageObjectRegistry()
 handle_urls = default_registry.handle_urls
