df68a248d827434e89ebed29ce1cd42b91ee427f
Mikhail Korobov <kmike84@gmail.com>
2022-05-26 23:53:42 +0500

    Update web_poet/page_inputs/browser.py
    Co-authored-by: Adrián Chaves <adrian@chaves.io>

--- web_poet/page_inputs/browser.py
+++ web_poet/page_inputs/browser.py
@@ -3,7 +3,7 @@ from web_poet.mixins import SelectableMixin
 
 class BrowserHtml(SelectableMixin, str):
     """ HTML returned by a web browser,
-    i.e. snapshot of the DOM tree in an HTML format.
+    i.e. snapshot of the DOM tree in HTML format.
     """
     def _selector_input(self) -> str:
         return self
