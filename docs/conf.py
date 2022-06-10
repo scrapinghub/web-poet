fff64aa4def52ed792691d1a8f8bd5e44f36e2bc
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-06-10 17:08:49 +0800

    fix linting issue on strings

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Scrapinghub"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = u'0.2.0'
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
