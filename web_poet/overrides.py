1afddc9be54218d6394b0c377cb65d3975964bcb
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-10-03 22:13:14 +0800

    import * from 'rules' in 'overrides'

--- web_poet/overrides.py
+++ web_poet/overrides.py
@@ -1,4 +1,6 @@
 import warnings
 
+from web_poet.rules import *  # noqa: F401, F403
+
 msg = "The 'web_poet.overrides' module has been moved into 'web_poet.rules'."
 warnings.warn(msg, DeprecationWarning, stacklevel=2)
