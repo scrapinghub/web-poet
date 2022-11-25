d3f1665469b4117d451231cf1e7f0b1e3c7a5b3c
Andrey Rakhmatullin <wrar@wrar.name>
2022-11-25 16:59:01 +0600

    The first version of serialization support.

---
+++ web_poet/serialization/__init__.py
@@ -0,0 +1,11 @@
+from . import functions  # needed to run register functions
+from .api import (
+    DeserializeFunction,
+    SerializedData,
+    SerializeFunction,
+    deserialize,
+    read_serialized_data,
+    register_serialization,
+    serialize,
+    write_serialized_data,
+)
