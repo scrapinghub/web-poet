829663566717c5e4d844f36d54afefe8c9e84e40
Andrey Rakhmatullin <wrar@wrar.name>
2023-01-03 17:46:15 +0400

    Add support for pytest < 7.

--- web_poet/testing/pytest.py
+++ web_poet/testing/pytest.py
@@ -6,6 +6,10 @@ import pytest
 
 from web_poet.testing.utils import OUTPUT_FILE_NAME, Fixture
 
+# https://github.com/pytest-dev/pytest/discussions/10261
+_version_tuple = getattr(pytest, "version_tuple", None)
+_new_pytest = _version_tuple and _version_tuple[0] >= 7
+
 
 class WebPoetFile(pytest.File):
     """Represents a directory containing test subdirectories for one Page Object."""
@@ -17,9 +21,10 @@ class WebPoetFile(pytest.File):
 
     def collect(self) -> Iterable[Union[pytest.Item, pytest.Collector]]:  # noqa: D102
         result: List[WebPoetItem] = []
-        for entry in self.path.iterdir():
+        path = self.path if _new_pytest else Path(self.fspath)
+        for entry in path.iterdir():
             if entry.is_dir():
-                item: WebPoetItem = WebPoetItem.from_parent(
+                item: WebPoetItem = _get_item(
                     self,
                     name=entry.name,
                     path=entry,
@@ -32,17 +37,32 @@ class WebPoetFile(pytest.File):
 class WebPoetItem(pytest.Item):
     """Represents a directory containing one test."""
 
-    def __init__(
-        self,
-        name,
-        parent=None,
-        config: Optional[pytest.Config] = None,
-        session: Optional[pytest.Session] = None,
-        nodeid: Optional[str] = None,
-        **kw
-    ) -> None:
-        super().__init__(name, parent, config, session, nodeid, **kw)
-        self.fixture = Fixture(self.path)
+    if _new_pytest:
+
+        def __init__(
+            self,
+            name,
+            parent=None,
+            config: Optional["pytest.Config"] = None,
+            session: Optional[pytest.Session] = None,
+            nodeid: Optional[str] = None,
+            **kw,
+        ) -> None:
+            super().__init__(name, parent, config, session, nodeid, **kw)
+            self.fixture = Fixture(self.path)
+
+    else:
+
+        def __init__(
+            self,
+            name,
+            parent=None,
+            config: Optional[Any] = None,
+            session: Optional[pytest.Session] = None,
+            nodeid: Optional[str] = None,
+        ) -> None:
+            super().__init__(name, parent, config, session, nodeid)
+            self.fixture = Fixture(Path(self.fspath, self.name))
 
     def runtest(self) -> None:  # noqa: D102
         self.fixture.assert_output()
@@ -51,8 +71,8 @@ class WebPoetItem(pytest.Item):
 _found_type_dirs: Set[Path] = set()
 
 
-def pytest_collect_file(
-    file_path: Path, path: Any, parent: pytest.Collector
+def collect_file_hook(
+    file_path: Path, parent: pytest.Collector
 ) -> Optional[pytest.Collector]:
     if file_path.name == OUTPUT_FILE_NAME:
         testcase_dir = file_path.parent
@@ -63,10 +83,50 @@ def pytest_collect_file(
         if type_dir in _found_type_dirs:
             return None
         _found_type_dirs.add(type_dir)
-        file: WebPoetFile = WebPoetFile.from_parent(
+        file: WebPoetFile = _get_file(
             parent,
             path=type_dir,
-            name=type_dir.name,
         )
         return file
     return None
+
+
+if _new_pytest:
+
+    def _get_item(parent: pytest.Collector, *, name: str, path: Path) -> WebPoetItem:
+        return WebPoetItem.from_parent(
+            parent,
+            name=name,
+            path=path,
+        )
+
+    def _get_file(parent: pytest.Collector, *, path: Path) -> WebPoetFile:
+        return WebPoetFile.from_parent(
+            parent,
+            path=path,
+        )
+
+    def pytest_collect_file(
+        file_path: Path, parent: pytest.Collector
+    ) -> Optional[pytest.Collector]:
+        return collect_file_hook(file_path, parent)
+
+else:
+    import py.path
+
+    def _get_item(parent: pytest.Collector, *, name: str, path: Path) -> WebPoetItem:
+        return WebPoetItem.from_parent(
+            parent,
+            name=name,
+        )
+
+    def _get_file(parent: pytest.Collector, *, path: Path) -> WebPoetFile:
+        return WebPoetFile.from_parent(
+            parent,
+            fspath=py.path.local(path),
+        )
+
+    def pytest_collect_file(
+        path: py.path.local, parent: pytest.Collector
+    ) -> Optional[pytest.Collector]:
+        return collect_file_hook(Path(path), parent)
