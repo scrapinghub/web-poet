6b2485d56e35b76b7006002d49e9fc01a32e497b
Adrián Chaves <adrian@chaves.io>
2023-01-05 08:31:41 +0100

    Fix example test for sync to_item

--- tests/test_example.py
+++ tests/test_example.py
@@ -9,16 +9,19 @@ def test_async_to_item():
     class Item:
         foo: str
 
-    @handle_urls("")
-    class Page(ItemPage[Item]):
-        @field
-        async def foo(self):
-            return "bar"
+    try:
 
-    item = get_item("file:///dev/null", Item)
-    assert isinstance(item, Item)
-    assert item.foo == "bar"
-    default_registry._rules.pop()
+        @handle_urls("")
+        class Page(ItemPage[Item]):
+            @field
+            async def foo(self):
+                return "bar"
+
+        item = get_item("file:///dev/null", Item)
+        assert isinstance(item, Item)
+        assert item.foo == "bar"
+    finally:
+        default_registry._rules.pop()
 
 
 def test_sync_to_item():
@@ -26,12 +29,15 @@ def test_sync_to_item():
     class Item:
         foo: str
 
-    @handle_urls("")
-    class Page(ItemPage):
-        def to_item(self):
-            return Item(foo="bar")
+    try:
 
-    item = get_item("file:///dev/null", Item)
-    assert isinstance(item, Item)
-    assert item.foo == "bar"
-    default_registry._rules.pop()
+        @handle_urls("")
+        class Page(ItemPage[Item]):
+            def to_item(self):
+                return Item(foo="bar")
+
+        item = get_item("file:///dev/null", Item)
+        assert isinstance(item, Item)
+        assert item.foo == "bar"
+    finally:
+        default_registry._rules.pop()
