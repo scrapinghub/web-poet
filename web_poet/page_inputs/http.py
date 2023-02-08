02290cef4fadd9076a6c4986248f175107320d41
Andrey Rakhmatullin <wrar@wrar.name>
2023-02-08 17:49:11 +0400

    Fix request_fingerprint.

--- web_poet/page_inputs/http.py
+++ web_poet/page_inputs/http.py
@@ -309,7 +309,7 @@ def request_fingerprint(req: HttpRequest) -> str:
     fp = sha1()
     fp.update(req.method.encode() + b"\n")
     fp.update(canonicalize_url(str(req.url)).encode())
-    fp.update(str(req.url).encode() + b"\n")
     for name, value in sorted(req.headers.items()):
         fp.update(f"{name}:{value}\n".encode())
+    fp.update(req.body)
     return fp.hexdigest()
