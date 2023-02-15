445eb78b480bb027a833c4d4f33bfd7747772541
Andrey Rakhmatullin <wrar@wrar.name>
2023-02-15 15:37:42 +0500

    Update web_poet/page_inputs/http.py
    Co-authored-by: Adrián Chaves <adrian@chaves.io>

--- web_poet/page_inputs/http.py
+++ web_poet/page_inputs/http.py
@@ -311,5 +311,6 @@ def request_fingerprint(req: HttpRequest) -> str:
     fp.update(canonicalize_url(str(req.url)).encode() + b"\n")
     for name, value in sorted(req.headers.items()):
         fp.update(f"{name.title()}:{value}\n".encode())
-    fp.update(b"\n" + req.body)
+    fp.update(b"\n")
+    fp.update(req.body)
     return fp.hexdigest()
