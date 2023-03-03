5112b877ce4dd98f61bbd0389cc4168f1c8ed969
Mikhail Korobov <kmike84@gmail.com>
2023-03-03 13:17:39 +0500

    Bump version: 0.8.0 → 0.8.1

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = "Zyte Group Ltd"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.8.1"
 
 
 # -- General configuration ---------------------------------------------------
