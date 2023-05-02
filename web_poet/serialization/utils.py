7f401c0767a86c14b4ff2647e9c224644a218c5d
Andrey Rakhmatullin <wrar@wrar.name>
2023-05-02 15:33:36 +0400

    Rename the exception type key.

--- web_poet/serialization/utils.py
+++ web_poet/serialization/utils.py
@@ -10,7 +10,7 @@ def _exception_to_dict(ex: Exception) -> Dict[str, Any]:
     Only the exception type and the first argument are saved.
     """
     return {
-        "type_name": _get_name_for_class(type(ex)),
+        "import_path": _get_name_for_class(type(ex)),
         "msg": ex.args[0] if ex.args else None,
     }
 
@@ -20,7 +20,7 @@ def _exception_from_dict(data: Dict[str, Any]) -> Exception:
 
     Only the exception type and the first argument are restored.
     """
-    exc_cls = load_class(data["type_name"])
+    exc_cls = load_class(data["import_path"])
     return exc_cls(data["msg"])
 
 
