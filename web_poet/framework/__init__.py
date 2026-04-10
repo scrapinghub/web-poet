74af45bc7eb02db46bc05e96e88aad32e3caf503
Adrian Chaves <adrian@zyte.com>
2026-04-10 11:04:03 +0200

    Keep the traceback on framework ImportError

--- web_poet/framework/__init__.py
+++ web_poet/framework/__init__.py
@@ -3,11 +3,11 @@
 try:
     import niquests  # noqa: F401
     from playwright.async_api import async_playwright  # noqa: F401
-except ImportError:
+except ImportError as exception:
     message = (
         "Could not import web_poet.framework dependencies. Install web-poet[framework]."
     )
-    raise ImportError(message) from None
+    raise ImportError(message) from exception
 
 from ._api import Framework, browser
 
