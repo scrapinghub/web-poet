1b34c3d92e04d48683c4394750b73a574a5fccc3
Adrian Chaves <adrian@zyte.com>
2026-06-16 11:59:45 +0200

    Solve pytest version issues

--- web_poet/testing/utils.py
+++ web_poet/testing/utils.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-from _pytest.assertion.util import assertrepr_compare
+from _pytest.assertion.util import (
+    assertrepr_compare,
+    dummy_highlighter,
+    get_assertion_text_diff_style,
+)
 
 if TYPE_CHECKING:
     import pytest
@@ -14,8 +18,15 @@ def comparison_error_message(
     """Generate an error message"""
     lines = [prefix] if prefix else []
 
-    explanation_lines = assertrepr_compare(
-        config=config, op=op, left=got, right=expected
+    explanation_lines = list(
+        assertrepr_compare(
+            op=op,
+            left=got,
+            right=expected,
+            verbose=getattr(config.option, "verbose", 0),
+            highlighter=dummy_highlighter,
+            assertion_text_diff_style=get_assertion_text_diff_style(config),
+        )
     )
     if explanation_lines:
         lines.extend(explanation_lines)
