d9c8a93b4f68b021bed027032a1c84f1686919f7
Andrey Rakhmatullin <wrar@wrar.name>
2026-06-18 14:44:11 +0500

    Add support for pytest 9.1.0 to comparison_error_message(). (#276)

--- web_poet/testing/utils.py
+++ web_poet/testing/utils.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
-from _pytest.assertion.util import assertrepr_compare
-
-if TYPE_CHECKING:
-    import pytest
+import _pytest.assertion.util as assertion_util
+import pytest
 
 
 def comparison_error_message(
@@ -14,9 +12,24 @@ def comparison_error_message(
     """Generate an error message"""
     lines = [prefix] if prefix else []
 
-    explanation_lines = assertrepr_compare(
-        config=config, op=op, left=got, right=expected
-    )
+    # assertrepr_compare() signature was changed in 9.1.0 in PRs 14418, 14425 and 14546
+    if pytest.version_tuple >= (9, 1, 0):
+        explanation_lines = list(
+            assertion_util.assertrepr_compare(
+                op=op,
+                left=got,
+                right=expected,
+                verbose=config.get_verbosity(pytest.Config.VERBOSITY_ASSERTIONS),
+                highlighter=assertion_util.dummy_highlighter,
+                assertion_text_diff_style=assertion_util.get_assertion_text_diff_style(
+                    config
+                ),
+            )
+        )
+    else:
+        explanation_lines = assertion_util.assertrepr_compare(  # type: ignore[call-arg,assignment]
+            config=config, op=op, left=got, right=expected
+        )
     if explanation_lines:
         lines.extend(explanation_lines)
     else:
